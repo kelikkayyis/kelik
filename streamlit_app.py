774974b69b93057409184558f39c3eb398c60ad9
Kelik Isbiyantoro <87562666+kelikkayyis@users.noreply.github.com>
2024-06-28 06:50:33 +0000

    add button

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,23 @@
 import streamlit as st 
-import plotly.express as px 
-import matplotlib.pyplot as plt
-import pandas as pd 
-import requests
-from st_aggrid import AgGrid
 
-#baca dataframe dari file csv 
-titanic = pd.read_csv('https://raw.githubusercontent.com/mofdac/-materi-das/main/01.%20Python%20for%20DA/titanic.csv')
 
 def main() : 
-    #matplotlib chart
-    st.subheader('Matplotlib')
-    fig,ax = plt.subplots()
-    plt.scatter(titanic['Age'],titanic['Fare'])
-    st.pyplot(fig)
-
-    st.subheader('Plotly')
-    plotly_fig = px.scatter(titanic['Age'],titanic['Fare'])
-    st.plotly_chart(plotly_fig)
+    click_me_btn = st.button('Click Me')
+    st.write(click_me_btn) #Return True kalo di Click 
+    check_btn = st.checkbox('Klik Jika Setuju')
+    if check_btn :
+        st.write('Anda Setuju')
+    
+    
+    radio_button= st.radio('Choose below',[x for x in range(1,3)])
+    st.write('Anda Memilih',radio_button)
+    
+    #slider 
+    age_slider = st.slider('Berapa Usia Anda',0,100)
+    st.write('Usia Anda',age_slider)
+    
+    #Input (Typing)
+    num_input = st.number_input('Input Berapapun')
+    st.write('Kuadrat dari {} adalah {}'.format(num_input,num_input**2))
 if __name__ == '__main__' : 
-    main()
+  main()
