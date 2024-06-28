9451fbb82be87889ccd1970f7597f95f15d47dd8
Kelik Isbiyantoro <87562666+kelikkayyis@users.noreply.github.com>
2024-06-28 06:45:42 +0000

    notebook test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,11 @@
 import streamlit as st 
-import plotly.express as px 
-import matplotlib.pyplot as plt
-import pandas as pd
-import requests
-from st_aggrid import AgGrid
-
-# house = pd.read_csv('https://raw.githubusercontent.com/kelikkayyis/kelik/main/house_clean.csv')
-titanic = pd.read_csv('https://raw.githubusercontent.com/mofdac/-materi-das/main/01.%20Python%20for%20DA/titanic.csv')
 
 def main() : 
-    click_me_btn = st.button('Click Me')
-    st.write(click_me_btn) #Return True kalo di Click 
-    check_btn = st.checkbox('Klik Jika Setuju')
-    if check_btn :
-        st.write('Anda Setuju')
-    
-    
-    radio_button= st.radio('Choose below',[x for x in range(1,3)])
-    st.write('Anda Memilih',radio_button)
-    
-    #slider 
-    age_slider = st.slider('Berapa Usia Anda',0,100)
-    st.write('Usia Anda',age_slider)
-    
-    #Input (Typing)
-    num_input = st.number_input('Input Berapapun')
-    st.write('Kuadrat dari {} adalah {}'.format(num_input,num_input**2))
+  st.header('This is Header')
+  st.subheader('This is SubHeader')
+  st.markdown('# Rendering Markdown ')
+  st.write('Some Phytagorean Equation : ')
+  st.latex('c^2 = a^2+b^2')
 
 if __name__ == '__main__' : 
   main()
