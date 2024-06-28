2fcd317013412505d16ac5d18ec1b19e9fefcdea
Kelik Isbiyantoro <87562666+kelikkayyis@users.noreply.github.com>
2024-06-28 06:51:20 +0000

    add col

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,30 @@
 import streamlit as st 
 
-
 def main() : 
-    click_me_btn = st.button('Click Me')
-    st.write(click_me_btn) #Return True kalo di Click 
-    check_btn = st.checkbox('Klik Jika Setuju')
-    if check_btn :
-        st.write('Anda Setuju')
+    #sidebar 
+    sidebar_checkbox = st.sidebar.checkbox('Checkbox di Sidebar')
+    sidebar_radio_button = st.sidebar.radio('Pilih Menu',options=['A','B','C'])
     
+    #columns :
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+       st.header("A cat")
+       st.image("https://static.streamlit.io/examples/cat.jpg")
+    #atau dengan assignment 
+    image_col1 = col1.image("https://static.streamlit.io/examples/cat.jpg")
+
+    with col2:
+       st.header("A dog")
+       st.image("https://static.streamlit.io/examples/dog.jpg")
+
+    with col3:
+       st.header("An owl")
+       st.image("https://static.streamlit.io/examples/owl.jpg")
+    #expander 
+    #dengan with atau dengan assignment 
+    expander = st.expander("Klik Untuk Detail ")
+    expander.write('Anda Telah Membuka Detail')
     
-    radio_button= st.radio('Choose below',[x for x in range(1,3)])
-    st.write('Anda Memilih',radio_button)
-    
-    #slider 
-    age_slider = st.slider('Berapa Usia Anda',0,100)
-    st.write('Usia Anda',age_slider)
-    
-    #Input (Typing)
-    num_input = st.number_input('Input Berapapun')
-    st.write('Kuadrat dari {} adalah {}'.format(num_input,num_input**2))
 if __name__ == '__main__' : 
   main()
