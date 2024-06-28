ef5b7b79724407a9c2095f1532e76ea7b89526b1
Kelik Isbiyantoro <87562666+kelikkayyis@users.noreply.github.com>
2024-06-28 06:27:44 +0000

    add button

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,21 +9,23 @@ from st_aggrid import AgGrid
 titanic = pd.read_csv('https://raw.githubusercontent.com/mofdac/-materi-das/main/01.%20Python%20for%20DA/titanic.csv')
 
 def main() : 
-  # st.header('This is Header')
-  # st.subheader('This is SubHeader')
-  # st.markdown('# Rendering Markdown ')
-  # st.write('Some Phytagorean Equation : ')
-  # st.latex('c^2 = a^2+b^2')
+    click_me_btn = st.button('Click Me')
+    st.write(click_me_btn) #Return True kalo di Click 
+    check_btn = st.checkbox('Klik Jika Setuju')
+    if check_btn :
+        st.write('Anda Setuju')
+    
+    
+    radio_button= st.radio('Choose below',[x for x in range(1,3)])
+    st.write('Anda Memilih',radio_button)
+    
+    #slider 
+    age_slider = st.slider('Berapa Usia Anda',0,100)
+    st.write('Usia Anda',age_slider)
+    
+    #Input (Typing)
+    num_input = st.number_input('Input Berapapun')
+    st.write('Kuadrat dari {} adalah {}'.format(num_input,num_input**2))
 
-  #matplotlib chart 
-  st.subheader('Matplotlib')
-  fig,ax = plt.subplots()
-  plt.scatter(titanic['Age'],titanic['Fare'])
-  st.pyplot(fig)
-
-  st.subheader('Plotly')
-  plotly_fig = px.scatter(titanic['Age'],titanic['Fare'])
-  st.plotly_chart(plotly_fig)
-  
 if __name__ == '__main__' : 
   main()
