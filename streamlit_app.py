dffc46febc1610e74429d3e899d6995bdd3535db
Kelik Isbiyantoro <87562666+kelikkayyis@users.noreply.github.com>
2024-06-28 06:48:20 +0000

    chart app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,22 @@
 import streamlit as st 
+import plotly.express as px 
+import matplotlib.pyplot as plt
+import pandas as pd 
+import requests
+from st_aggrid import AgGrid
+
+#baca dataframe dari file csv 
+titanic = pd.read_csv('https://raw.githubusercontent.com/mofdac/-materi-das/main/01.%20Python%20for%20DA/titanic.csv')
 
 def main() : 
-  st.header('This is Header')
-  st.subheader('This is SubHeader')
-  st.markdown('# Rendering Markdown ')
-  st.write('Some Phytagorean Equation : ')
-  st.latex('c^2 = a^2+b^2')
+    #matplotlib chart
+    st.subheader('Matplotlib')
+    fig,ax = plt.subplots()
+    plt.scatter(titanic['Age'],titanic['Fare'])
+    st.pyplot(fig)
 
+    st.subheader('Plotly')
+    plotly_fig = px.scatter(titanic['Age'],titanic['Fare'])
+    st.plotly_chart(plotly_fig)
 if __name__ == '__main__' : 
-  main()
+    main()
