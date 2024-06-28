550a6b134b086956e8ee5978015de4f913d98b7e
Kelik Isbiyantoro <87562666+kelikkayyis@users.noreply.github.com>
2024-06-28 04:10:23 +0000

    dataframe csv add

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,16 @@
 import streamlit as st 
-import pandas as pd 
-import requests
-from st_aggrid import AgGrid
+import pandas as pd
 
-#baca dataframe dari file csv 
 house = pd.read_csv('https://raw.githubusercontent.com/kelikkayyis/kelik/main/house_clean.csv')
 
-#read json file dari data covid 
-flight_passanger_api = requests.post('https://forecastpassengerapi.herokuapp.com/forecast_timeseries',json={
-  "month_limit": "2020-01-01",
-  "window_size": 12
-}).json()
-
 def main() : 
-  
-  st.write('Contoh dataframe')
-  st.dataframe(house)
+  st.header('This is Header')
+  st.subheader('This is SubHeader')
+  st.markdown('# Rendering Markdown ')
+  st.write('Some Phytagorean Equation : ')
+  st.latex('c^2 = a^2+b^2')
 
-  st.write('Contoh JSON')
-  st.json(flight_passanger_api)
-  st.write('Metrics')
-  st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
-  
-  st.write('Menampilkan Dataframe dengan St AgGrid')
-  AgGrid(house)
-  st.table([x for x in range(1,5)])
+  st.dataframe(house)
   
 if __name__ == '__main__' : 
   main()
