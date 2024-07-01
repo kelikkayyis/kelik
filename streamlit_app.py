57e43f133e5ef647ee2edf67f03749ee3a59d4cd
Kelik Isbiyantoro <87562666+kelikkayyis@users.noreply.github.com>
2024-07-01 06:13:38 +0000

    width

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ def main():
     iris_data['species'] = [iris.target_names[i] for i in iris.target]
 
     # Pilihan halaman dengan tabs
-    st.sidebar.image("lpdp.png")
+    st.sidebar.image("lpdp.png", width=150)
     tab = st.sidebar.radio("Pilih halaman:", ["Iris Dataset", "Visualisasi", "Analisis", "Prediksi"])
 
     # Halaman untuk Show Data dengan filter
